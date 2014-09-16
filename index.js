cfd0639ec1d22c1a66ab3e60056ef8c32096bcb2
camshaft <bytheway.cameron@gmail.com>
2014-09-16 14:47:32 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  componentDidMount: function() {
+    // TODO lazy-load the facebook sdk
+  },
+  render: function() {
+    return '';
+  }
+});
